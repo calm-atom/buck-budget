c3c669876dd122711e48519332379080659f4640
John Buckley <jbuckleyv@gmail.com>
2023-12-04 10:33:32 -0600

    Setup template directory and home template

--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to Buck Budget</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	tmplPath := filepath.Join("templates", "home.html")
+	tmpl, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		log.Printf("Parsing template %v", err)
+		http.Error(w, "Failed to parse template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("Executing template %v", err)
+		http.Error(w, "Failed to execute template", http.StatusInternalServerError)
+		return
+
+	}
 }
 
 func main() {
